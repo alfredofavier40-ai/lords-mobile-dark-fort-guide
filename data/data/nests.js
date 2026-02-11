fcdeaa4a97ddfaf79d9a0d0078c77fbba2db7632
alfredofavier40-ai <alfredofavier40@gmail.com>
2026-02-11 05:15:03 -0500

    Create nests.js

---
+++ data/data/nests.js
@@ -0,0 +1,5 @@
+const nests = {
+  inf: { counter:"inf", formation:"Infantry Phalanx" }, // Infantería vence a Rango
+  range: { counter:"range", formation:"Ranged Phalanx" }, // Rango vence a Caballería
+  cav: { counter:"cav", formation:"Cavalry Phalanx" }    // Caballería vence a Infantería
+};
